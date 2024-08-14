c68191dfdc53577bee471cb73cac1fac991a26cd
Nguyen Duy <wuuyi123@gmail.com>
2024-08-14 12:12:59 +0700

    update gen.bat

--- lib/gen.bat
+++ lib/gen.bat
@@ -1,11 +1,28 @@
 @echo off
-rem run in vs command prompt
+setlocal enabledelayedexpansion
+
+set SPACES=  
+
+set libname=libcef
+
+set input=win\%libname%.dll
+set output=win\%libname%.lib
+
+set dump_file=tmp\%libname%.dump
+set def_file=tmp\%libname%.def
 
 mkdir tmp
+dumpbin /EXPORTS %input% > %dump_file%
 
-dumpbin /EXPORTS libName.dll > tmp\libName.exports
-rem TODO: make exports.def file
+echo LIBRARY > %def_file%
+echo EXPORTS >> %def_file%
 
-lib /def:tmp/exports.def /machine:x64 /out:libName.lib
+for /F "tokens=4" %%A in ('findstr "cef_" %dump_file%') do (
+    @echo %SPACES%%%A >> %def_file%
+)
 
+lib /def:%def_file% /machine:x64 /out:%output%
+
+del /s /q win\*.exp
+del /s /q tmp\*
 rmdir tmp
